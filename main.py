7a14b6a342280db0460a626b9ef1188e106718e2
101OZONE <35305360+101OZONE@users.noreply.github.com>
2020-07-03 03:30:51 +0800

    obtain emulator serial number, connect emulator by serial number and start game by serial number

--- main.py
+++ main.py
@@ -4,31 +4,75 @@ import os
 def m_tap(x, y, name):
     print(name)
     print(x, y)
-    com = 'adb -s' + name + 'shell input tap %s %s' % (x, y)
+    cmd = 'adb -s' + name + 'shell input tap %s %s' % (x, y)
     try:
-        os.system(com)
+        os.system(cmd)
     except:
         print('click fail' + name)
         exit(1)
-    print(com)
+    print(cmd)
 
 
 def m_swipe(x1, y1, x2, y2, duration, name):
     print(x1, y1, x2, y2)
-    com = 'adb -s' + name + 'shell input swipe %s %s %s %s %s' % (x1, y1, x2, y2, duration)
+    cmd = 'adb -s' + name + 'shell input swipe %s %s %s %s %s' % (x1, y1, x2, y2, duration)
     try:
-        os.system(com)
+        os.system(cmd)
     except:
         print('swipe fail' + name)
         exit(1)
-    print(com)
+    print(cmd)
 
 
 def m_text(s, name):
-    com = 'adb -s' + name + 'shell input text %s' % s
+    cmd = 'adb -s' + name + 'shell input text %s' % s
     try:
-        os.system(com)
+        os.system(cmd)
     except:
         print('text fail' + name)
         exit(1)
-    print(com)
+    print(cmd)
+
+
+def getSerialNo():
+    cmd = 'adb devices'
+    try:
+        os.system(cmd)
+    except:
+        print('get serial number fail')
+        exit(1)
+    r = os.popen(cmd)
+    r.readline()
+    i = 0
+    for line in r.readlines():
+        name = line.split()
+        if (len(name) > 0):
+            nameList[i] = name[0]
+            i += 1
+
+
+def m_connect(name):
+    cmd = 'adb connect %s' % name
+    try:
+        os.system(cmd)
+    except:
+        print('connect %s fail' % name)
+        exit(1)
+
+
+def startGame(name):
+    cmd = 'adb -s %s shell am start -n %s' % (name, pcrActivityName)
+    try:
+        os.system(cmd)
+    except:
+        print('%s start game fail' % name)
+        exit(1)
+
+
+if __name__ == '__main__':
+    nameList = [0, 0, 0, 0]
+    pcrActivityName = 'com.bilibili.priconne/com.bilibili.princonne.bili.MainActivity'
+    getSerialNo()
+    print(nameList)
+    m_connect(nameList[0])
+    startGame(nameList[0])
